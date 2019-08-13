0a8d434b31ee4144b7fd4c10e989cd02da02bd43
EllieJudge <judge_ellie@hotmail.com>
2019-08-13 15:06:37 +0100

    Updates

---
+++ precache-manifest.92fd2b2661245351e48bf474c449d51d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3aec41c0c9e795f71756763360f48c",
+    "url": "/Todo-List-app/index.html"
+  },
+  {
+    "revision": "9e021c0a5816c2f89878",
+    "url": "/Todo-List-app/static/js/2.4c72dff4.chunk.js"
+  },
+  {
+    "revision": "90a6939b135863a8c7cb",
+    "url": "/Todo-List-app/static/js/main.abb355af.chunk.js"
+  },
+  {
+    "revision": "5cf5dad9f7d5c3e8289e",
+    "url": "/Todo-List-app/static/js/runtime~main.91c68b61.js"
+  },
+  {
+    "revision": "a3addc7fa63090f19eb1ea3c9e482551",
+    "url": "/Todo-List-app/static/media/ginge.a3addc7f.png"
+  }
+]);
